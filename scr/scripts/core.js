b54b1d3732aab340a92d06da42a8aa3bb1275fe0
Jose Fernando Casadiego Mora <jose.fernando.casadiego@gmail.com>
2023-02-28 20:54:55 -0500

    More opcions to the fiel, js addevenlisteners to boxes

--- scr/scripts/core.js
+++ scr/scripts/core.js
@@ -1,10 +1,12 @@
 /*--------------------------card movement-----------------------*/
 const cards = document.querySelectorAll('.cards');
-const newCardTurn = document.querySelector(".newCard")
+const newCardTurn = document.querySelector(".newCard");
+const discartCart = document.querySelector(".cementery");
+const boxes = document.querySelectorAll(".box");
 
 
-cards.forEach(card => {
-    
+cards.forEach(card => {   
+   
     card.addEventListener("mouseenter", function(e){
         card.style.backgroundColor="white"
     })
@@ -14,17 +16,48 @@ cards.forEach(card => {
     })
 });
 
+boxes.forEach(box =>{
+
+    box.addEventListener("dragover",e =>{
+        e.preventDefault();        
+        console.log("casa")
+    })
+
+    box.addEventListener("drop",e =>{
+        box.appendChild(cards)
+    })
+
+
+})
+
+
+discartCart.addEventListener("mouseenter",function(e){
+    const text=document.querySelector(".cementery p");
+    discartCart.style.backgroundColor="rgb(230, 12, 175)";
+    text.innerHTML="DESCARTAR <br/> MANO";
+})
+
+discartCart.addEventListener("mouseleave",function(e){
+    const text=document.querySelector(".cementery p");
+    discartCart.style.backgroundColor="grey";
+    text.innerHTML="";
+})
+
+
 newCardTurn.addEventListener("mouseenter",function(e){
     const text = document.querySelector(".newCard p")
-    newCardTurn.style.backgroundColor="rgb(76, 184, 14)"
+    newCardTurn.style.backgroundColor="rgb(76, 184, 14)";
     text.innerHTML="NUEVO <br/> TURNO";
     
 })   
  
 newCardTurn.addEventListener("mouseleave",function(e){
-    const text = document.querySelector(".newCard p")
+    const text = document.querySelector(".newCard p");
     newCardTurn.style.backgroundColor="grey"
     text.innerHTML="";
 })   
  
+
+
+
 
